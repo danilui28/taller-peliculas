e1537165b851323ed880884ca4f1c50cc5608049
danilui28 <david82.macias@gmail.com>
2026-07-15 16:52:02 -0500

    cambios de la parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react';
 import './App.css'
 
+function Pelicula({titulo}){
+
+  return(
+    <div style={{
+      border: '1px solid gray',
+      padding: '10px',
+      margin: '10px',
+      borderRadius: '25px',
+      }}
+      >
+      <p>{titulo}</p>
+    </div>
+  )
+}
+
 function App() {
 
   return (
     <div>
       <h1>Mis peliculas favoritas</h1>
+      <Pelicula titulo="Spiderman"></Pelicula>
+      <Pelicula titulo="Backrooms"></Pelicula>
+      <Pelicula titulo="Matrix"></Pelicula>
+      <Pelicula titulo="Rápidos y furiosos"></Pelicula>
     </div>
   )
 }
